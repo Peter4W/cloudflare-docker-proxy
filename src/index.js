108149f7bcb5b048ea8e32c22be87f2c7e0bfe50
Peter4W <167377513+Peter4W@users.noreply.github.com>
2024-09-20 09:14:13 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.windsnow.top": dockerHub,
+  "quay.windsnow.top": "https://quay.io",
+  "gcr.windsnow.top": "https://gcr.io",
+  "k8s-gcr.windsnow.top": "https://k8s.gcr.io",
+  "k8s.windsnow.top": "https://registry.k8s.io",
+  "ghcr.windsnow.top": "https://ghcr.io",
+  "cloudsmith.windsnow.top": "https://docker.cloudsmith.io",
+  "ecr.windsnow.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.windsnow.top": dockerHub,
 };
 
 function routeByHosts(host) {
